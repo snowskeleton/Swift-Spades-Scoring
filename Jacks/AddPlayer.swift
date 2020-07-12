920d8eecc1e7bda2be49aa24acf031dbe1d39544
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-07-12 19:38:07 -0400

    Leaving this for now. Moving on.

--- Jacks/AddPlayer.swift
+++ Jacks/AddPlayer.swift
@@ -5,35 +5,44 @@
 //  Copyright © 2020 Blizzard Skeleton. All rights reserved.
 
 import SwiftUI
+import Combine
 
 struct AddPlayer: View {
     @EnvironmentObject var player: PlayerList
     @EnvironmentObject var team: TeamList
+    @State private var keyboardHeight: CGFloat = 0
+    @State private var hideSave = false
+    
     @Binding var show: Bool
-
+    
     
     var body: some View {
         NavigationView {
             VStack {
                 VStack {
                     Form {
-                        TextField("Player 1", text: $player.list[0])
-                            .padding()
-                        TextField("Player 2", text: $player.list[1])
-                            .padding()
+                        Section(header: Text("Players")) {
+                            TextField("Player 1", text: $player.list[0])
+                                .padding()
+                            TextField("Player 2", text: $player.list[1])
+                                .padding()
+                            
+                            TextField("Player 3", text: $player.list[2])
+                                .padding()
+                            TextField("Player 4", text: $player.list[3])
+                                .padding()
+                        }
+                        Section(header: Text("Teams")) {
+                            TextField("Team 1 name", text: $team.list[0])
+                                .padding()
+                            TextField("Team 2 name", text: $team.list[1])
+                                .padding()
+                        }
                         
-                        TextField("Player 3", text: $player.list[2])
-                            .padding()
-                        TextField("Player 4", text: $player.list[3])
-                            .padding()
-                        
-                        TextField("Team 1 name", text: $team.list[0])
-                            .padding()
-                        TextField("Team 2 name", text: $team.list[1])
-                            .padding()
                     }
-                }
-                
+                }.padding(.bottom, keyboardHeight)
+                    
+
                 Button(action: {
                     self.player.colors.shuffle()
                     self.show = false
@@ -42,12 +51,13 @@ struct AddPlayer: View {
                         .bold()
                         .padding()
                 }
+                
             }.navigationBarTitle("Add Players")
             
         }
+        .onReceive(Publishers.keyboardHeight) { self.keyboardHeight = $0 }
     }
 }
-
 
 struct AddPlayer_Previews: PreviewProvider {
     static var previews: some View {
